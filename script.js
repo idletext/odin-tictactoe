3a067e0d32345f9991ad06dfb0c0b01441448d89
idletext <210270082+idletext@users.noreply.github.com>
2025-11-13 14:34:11 +0800

    Build multiple functions in gameLogic and playerModule
    - checkWin module, checkTie module
    - makePlayer module

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-const game = (function(){
+const gameLogic = (function(){
     
     function makeBoard (size) {
         return Array.from({length : size}, () => Array(size).fill(null));
     };
-
-    const board = makeBoard(3)
     
     function makeMove (board, row, col, player ){
         if (!board[row][col]) {
             board[row][col] = player;
-            //add return function here to indicate successful placement
-        } 
-        //add return function here to indicate unsuccessful placement
-    }
+            
+        }    
+    };
     
-    function checkWin () {
+    function checkWin (board, player) {
+        const size = board.length;
+        //check rows
+        for (let row = 0; row < size; row++) {
+            if ( board[row].every( (cell) => cell === player) ) return true;
+        };
+        //check columns
+        for (let col = 0; col < size; col++) {
+            if ( board.every( (row) => row[col] === player ) ) return true;
+        };
+        //check diagonals
+        const mainDiagonal = board.every( (row, i => row[i] === player) )
+        const antiDiagonal = board.every( (row, i) => (row - 1 - i) === player )
+
+        return mainDiagonal || antiDiagonal;
+    };
+
+    function checkTie (board) {
+
+    return board.every(rows => rows.every(elem => elem !== null)) 
         
     };
 
-    return {
-        
-    }
+    return{ makeBoard, makeMove, checkWin, checkTie}
 
 })();
 
 const playerModule = (function(){
 
+    function makePlayer (name){
+        let symbol = "";
+        let score = 0;
+
+        const getName = () => name;
+        const giveSymbol = (input) => symbol = input;
+        const getSymbol = () => symbol;
+        const getScore = () => score;
+        const giveScore = () => score++;
+
+        return { getName, giveSymbol, getSymbol, getScore, giveScore }
+    };
+    
+
+    return {
+        makePlayer
+    };
 })();
 
 const stats =  (function(){
 
-})();  
+
+})();
+
+
+
+
+
+
