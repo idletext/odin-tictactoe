3b6373329ccfc7f85f877825b9f18331b99cbac9
idletext <210270082+idletext@users.noreply.github.com>
2025-11-13 09:26:14 +0800

    Build makeMove function under game module

--- script.js
+++ script.js
@@ -4,13 +4,27 @@ const game = (function(){
         return Array.from({length : size}, () => Array(size).fill(null));
     };
 
+    const board = makeBoard(3)
+    
+    function makeMove (board, row, col, player ){
+        if (!board[row][col]) {
+            board[row][col] = player;
+            //add return function here to indicate successful placement
+        } 
+        //add return function here to indicate unsuccessful placement
+    }
+    
+    function checkWin () {
+        
+    };
+
     return {
         
     }
 
 })();
 
-const player = (function(){
+const playerModule = (function(){
 
 })();
 
