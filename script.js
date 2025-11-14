eec830b3a245d1cb4be9709e7bb82eb2b0596874
idletext <210270082+idletext@users.noreply.github.com>
2025-11-15 04:35:47 +0800

    Refactor old round function spaghetti code
    - round works properly now.

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const gameLogic = (function(){
         return {newRound, getRound};
     };
     
-    function applyMove (board, row, col, playerSymbol ){
+    function applyMove (board, row, col, playerSymbol){
         if (!board[row][col]) {
             board[row][col] = playerSymbol;
             return true;
@@ -63,10 +63,10 @@ const gameLogic = (function(){
             if ( board.every( (row) => row[col] === player ) ) return true;
         };
         //check diagonals
-        const mainDiagonal = board.every( (row, i) => (row[i] === player) )
-        const antiDiagonal = board.every( (row, i) => (row - 1 - i) === player )
-
-        return mainDiagonal || antiDiagonal;
+        const mainDiagonal = board.every((row, i) => row[i] === player);
+        const antiDiagonal = board.every((row, i) => row[size - 1 - i] === player);
+      
+        return mainDiagonal || antiDiagonal;;
     };
 
     function checkTie (board) {
@@ -88,29 +88,82 @@ const gameLogic = (function(){
         console.log(board[2].toString());
     };
 
-    async function playerMove(board, player, input) { 
+    async function movePrompt(){
+        while(true) {
+            const input = await nodejs.input().ask("To pick, enter two numbers that represent's 'row' and 'col' (ex: 0 2 ):");
+            const parts = input.split(" ").map(Number);
 
-        const parts = input.split(" ").map(Number);
+            if ( parts.length !== 2 || parts.some( n => isNaN(n) ) ) {
 
-        if ( parts.length !== 2 || parts.some( n => isNaN(n) ) ) {
+                console.log("Invalid input. Try again." )
+                continue;
 
-            console.log("Invalid input. Try again." );
-            return false(); // I believe this is called a recursion?
-        }
+            };
 
-        const [x, y] = parts;
-        const apply = gameLogic.applyMove(board, x, y, player.getSymbol());
+        return parts;
+            
+        };
+    };
 
-        if (apply === true) {
-            console.log("Input Success!");
-        }
+    async function playerMove (board, player) {
+        io.playerUp(player)
+        gameLogic.renderBoard(board);
+        let running = true;
 
-        if (apply === false) {
-            console.log("spot is taken! Enter number again!");
-            return false;
-        }
+        while(running) {
+            const prompt = await gameLogic.movePrompt();
+            const [x, y] = prompt;
+
+            const apply = gameLogic.applyMove(board, x, y, player.getSymbol());
+
+            if (apply === true){
+                console.log("Input Success")
+                break;  
+            } else if(apply === false) {
+                console.log("spot is taken! Enter number again.");
+            };
+        };
 
     };
+    
+    function checkState(board, player) {
+        const trueWin = gameLogic.checkWin(board, player.getSymbol()) === true;
+        const trueTie = gameLogic.checkTie === true;
+
+            if (trueWin) {
+                io.shoutRoundWinner(player);
+                return true;
+            }
+
+            if (trueTie) {
+                io.shoutTie();
+                return true;
+            };
+       
+         };    
+
+         const round = async function(player1, player2) {
+            thisBoard = gameLogic.makeBoard(3)
+            io.shoutRound()
+            let running = true;
+    
+            while(running){
+    
+            await gameLogic.playerMove(thisBoard, player1);
+    
+            if (gameLogic.checkState(thisBoard, player1) === true){ 
+                return true;
+            };
+    
+            await gameLogic.playerMove(thisBoard, player2);
+    
+            if (gameLogic.checkState(thisBoard, player2) === true){ 
+                return true;
+            };
+    
+        };
+    
+        };
 
     return { 
         makeBoard, 
@@ -119,9 +172,12 @@ const gameLogic = (function(){
         checkTie, 
         gameFinish, 
         gameRound, 
-        renderBoard, 
-        playerMove
-    }
+        renderBoard,
+        movePrompt,
+        playerMove,
+        checkState,
+        round,
+    };
 
 })();
 
@@ -154,12 +210,33 @@ const info = (function(){
 
 })();
 
+const io = (function() {
+    const shoutRound = (input) => console.log(`Round ${input}`)
 
-// round
-const gamePlay = (async function() {
-    
+    const playerUp = (player) => console.log(`${player.getName()} ${player.getSymbol()}, you're up!`)
 
-    // Ask Player's information
+    const invalidInput = () => console.log("Invalid Input!") 
+
+    const shoutGiveScore = (player) => console.log( `A score for ${player.getName()}!` );
+
+    const shoutRoundWinner = (player) => console.log( `${player.getName()} wins!`)
+
+    const shoutTie = () => console.log("It's a tie!");
+
+    return{
+        shoutRound,
+        playerUp,
+        invalidInput,
+        shoutGiveScore,
+        shoutRoundWinner,
+        shoutTie,
+    }
+})();
+
+
+
+const gameStart = (async function () {
+
     const playerOneName = await nodejs.input().ask("player 1, what is your name?");
 
     const playerOne = playerModule.makePlayer(playerOneName);
@@ -175,64 +252,105 @@ const gamePlay = (async function() {
      //start round
     const thisRound = gameLogic.gameRound();
     thisRound.newRound();
-    round()
 
-    function round () {
+    gameLogic.round(playerOne, playerTwo);
 
-    //Start Round
-    // let thisBoard = gameLogic.makeBoard(3);
-    let thisBoard = [ ["o", "o", "x"], ["x", null, "x"], ["o", "o", null] ];
     
-    console.log("let's start the game!")
-    console.log(`round ${thisRound.getRound()}`)
+
+}) ();
+
+
+
+
+
+// round
+// const gamePlay = (async function() {
+    
+
+//     async function playerMove(board, player, input) { 
+
+//         const parts = input.split(" ").map(Number);
+
+//         if ( parts.length !== 2 || parts.some( n => isNaN(n) ) ) {
+
+//             console.log("Invalid input. Try again." );
+//             return false(); // I believe this is called a recursion?
+//         }
+
+//         const [x, y] = parts;
+//         const apply = gameLogic.applyMove(board, x, y, player.getSymbol());
+
+//         if (apply === true) {
+//             console.log("Input Success!");
+//         }
+
+//         if (apply === false) {
+//             console.log("spot is taken! Enter number again!");
+//             return false;
+//         }
+
+//     };
+
+    // // Ask Player's information
+
+    // round()
+
+    // function round () {
+
+    // //Start Round
+    // // let thisBoard = gameLogic.makeBoard(3);
+    // let thisBoard = [ ["o", "o", "x"], ["x", null, "x"], ["o", "o", null] ]; // Test
+    
+    // console.log("let's start the game!")
+    // console.log(`round ${thisRound.getRound()}`)
     
     
-    moveLoop(playerOne, playerTwo, thisBoard);
+    // moveLoop(playerOne, playerTwo, thisBoard);
     
     
-    function playerMoveInput(player, board, playerInput) {
+    // function playerMoveInput(player, board, playerInput) {
 
-        const check = gameLogic.playerMove(board, player, playerInput);
+    //     const check = gameLogic.playerMove(board, player, playerInput);
 
-        if (check === false) playerMoveInput(player.getSymbol()); //repeat loop
+    //     if (check === false) playerMoveInput(player.getSymbol()); //repeat loop
 
-        if (gameLogic.checkTie(board, player) === true){
-            thisRound.newRound()
-            console.log("it's a tie! Next Round!")
-            return true;
-          };
+    //     if (gameLogic.checkTie(board, player) === true){
+    //         thisRound.newRound()
+    //         console.log("it's a tie! Next Round!")
+    //         return true;
+    //       };
 
-        if (gameLogic.checkWin(board, player.getSymbol()) === true){
-            playerOne.giveScore()
-            console.log(`Score to ${player.getName()}!`)
-            console.log(playerOne.getScore())
-            console.log(playerTwo.getScore())
-            if (thisRound)
-            thisRound.newRound()
-            console.log("Next Round!")
-            round();
-          };
-    };
+    //     if (gameLogic.checkWin(board, player.getSymbol()) === true){
+    //         playerOne.giveScore()
+    //         console.log(`Score to ${player.getName()}!`)
+    //         console.log(playerOne.getScore())
+    //         console.log(playerTwo.getScore())
+    //         if (thisRound)
+    //         thisRound.newRound()
+    //         console.log("Next Round!")
+    //         round();
+    //       };
+    // };
 
-    async function moveLoop(player1, player2, board) {
-        for(i = 0; i < thisBoard.length * 3; i++) {
+    // async function moveLoop(player1, player2, board) {
+    //     for(i = 0; i < thisBoard.length * 3; i++) {
         
-        gameLogic.renderBoard(thisBoard);
-        console.log(`${player1.getName()} ${player1.getSymbol()} you're up!`);
-        const playerInput = await nodejs.input().ask("To pick, enter two numbers that represents 'row' and 'col' (ex: 0 2 ):");
-        playerMoveInput(player1, board, playerInput);
+    //     gameLogic.renderBoard(thisBoard);
+    //     console.log(`${player1.getName()} ${player1.getSymbol()} you're up!`);
+    //     const playerInput = await nodejs.input().ask("To pick, enter two numbers that represents 'row' and 'col' (ex: 0 2 ):");
+    //     playerMoveInput(player1, board, playerInput);
         
-        gameLogic.renderBoard(thisBoard);
-        console.log(`${player2.getName()} ${player2.getSymbol()} you're up!`);
-        const playerInput2 = await nodejs.input().ask("To pick, enter two numbers that represents 'row' and 'col' (ex: 0 2 ):");
-        playerMoveInput(player2, board, playerInput2);
-        
-        };
-    };
+    //     gameLogic.renderBoard(thisBoard);
+    //     console.log(`${player2.getName()} ${player2.getSymbol()} you're up!`);
+    //     const playerInput2 = await nodejs.input().ask("To pick, enter two numbers that represents 'row' and 'col' (ex: 0 2 ):");
+    //     playerMoveInput(player2, board, playerInput2);
 
-    };//Round
+    //     };
+    // };
 
-})();
+    // };//Round
+
+
 
 // Tests (TO BE REMOVED)
 
