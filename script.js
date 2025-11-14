2c53c4a989b3e77954aff804d6f2e6cb74be69ef
idletext <210270082+idletext@users.noreply.github.com>
2025-11-14 15:13:26 +0800

    Build renderBoard & playerMove factory funcions
    - RenderBoard logs the state of the board on the cosole.
    - PlayerMove provides and validates player input and
      passes it to the makeMove function.
    - Also decided to test this using node.js for now.

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const gameLogic = (function(){
+const readline = require("readline");
+const nodejs = (function() {
+    // This persists inside the closure
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+    function input() {
+      // Returns a promise that resolves the user's input
+      const ask = (question) => {
+        return new Promise((resolve) => {
+          rl.question(question, (answer) => resolve(answer));
+        });
+      };
     
+      // Clean up the interface
+      const close = () => rl.close();
+    
+      // Public API
+      return {
+        ask,
+        close
+      };
+    };
+    
+    return {input};
+
+})();
+
+const gameLogic = (function(){
+
     function makeBoard (size) {
         return Array.from({length : size}, () => Array(size).fill(null));
     };
@@ -13,9 +43,9 @@ const gameLogic = (function(){
         return {newRound, getRound};
     };
     
-    function makeMove (board, row, col, player ){
+    function makeMove (board, row, col, playerSymbol ){
         if (!board[row][col]) {
-            board[row][col] = player;
+            board[row][col] = playerSymbol;
             return true;
         };
         return false;
@@ -51,7 +81,39 @@ const gameLogic = (function(){
         console.log ( `${losePlayer}'s Score: ${losePlayer.getScore()}`);
     };
 
-    return { makeBoard, makeMove, checkWin, checkTie, gameFinish, gameRound}
+    function renderBoard(board) {
+        console.log(board[0].toString());
+        console.log(board[1].toString());
+        console.log(board[2].toString());
+    };
+
+    async function playerMove(board, player) { 
+        console.log("player 1 'x' you're up!")
+        const playerInput = await nodejs.input().ask("To pick, enter two numbers that represents 'row' and 'col' (ex: 3 5 ):");
+
+        const parts = playerInput.split(" ").map(Number);
+
+        if ( parts.length !== 2 || parts.some( n => isNaN(n) ) ) {
+
+            console.log("Invalid input. Try again." );
+            return playerMove(); // I believe this is called a recursion?
+        }
+
+        const [x, y] = parts;
+        gameLogic.makeMove(board, x, y, player.getSymbol());
+
+    };
+
+    return { 
+        makeBoard, 
+        makeMove, 
+        checkWin, 
+        checkTie, 
+        gameFinish, 
+        gameRound, 
+        renderBoard, 
+        playerMove
+    }
 
 })();
 
@@ -77,22 +139,63 @@ const playerModule = (function(){
 })();
 
 const stats =  (function(){
-
-
-})();
-
-const screenController = ( function(){
-
 })();
 
 
-const gamePlay = (function() {
+// Console Game Play Testing
+const gamePlay = (async function() {
+
+    // const prompt = nodejs.input();
+    let thisBoard = gameLogic.makeBoard(3);
+    const thisRound = gameLogic.gameRound();
+
+    const playerOneInput = await nodejs.input().ask("player 1, what is your name?");
+
+    const playerOne = playerModule.makePlayer(playerOneInput);
+    playerOne.giveSymbol('x')
+    console.log(`Hello ${playerOne.getName()}!`)
+
+    const playerTwoInput = await nodejs.input().ask("player 2, what is your name?");
+    // nodejs.input().close()
+
+    const playerTwo = playerModule.makePlayer(playerTwoInput);
+    playerTwo.giveSymbol('o')
+    console.log(`Hello ${playerTwo.getName()}!`)
+
+    console.log("let's start the game!")
+
+    gameLogic.renderBoard(thisBoard);
     
+    //playerMove
+
+    // function checkState(board){
+    //     if (board.checkTie())
+    // }
+
+    await gameLogic.playerMove(thisBoard, playerOne);
+
+    gameLogic.renderBoard(thisBoard)
+
     
+
+    // move loop
+    // for(i = 0; i < thisBoard.length * 3; i++) {
+    //     renderBoard(thisBoard)
+    //     console.log("Player 1 your're up")
+    //     console.log('Pick a side eg. "r1"')    
+    // thisBoard = [ ["x", "x", "x"], ["x", "x", "x"], ["x", "x", "x"] ];
+    
+    // };
+
+    console.log("gameFinished");
+        
 })();
 
 // Tests (TO BE REMOVED)
 
+// [ ["x", "x", "x"], ["x", "x", "x"], ["x", "x", "x"] ]
+
+// [ ["x", "x", "x"], ["x", "x", "x"], ["x", "x", "null"] ]
 
 
 
